954b4c36c5c3b6bdd2bc04b881956bf76f6bca8f
MJ Lai <mjlai93@gmail.com>
2021-10-16 10:06:13 +0800

    [] Project setup.

---
+++ assessment/Module/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assessment
+//
+//  Created by Hexa-MingJie.Lai on 16/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ assessment/Service/APIService.swift
@@ -0,0 +1,8 @@
+//
+//  APIService.swift
+//  assessment
+//
+//  Created by Hexa-MingJie.Lai on 16/10/2021.
+//
+
+import Foundation
